8b236dbe91ab77fc45b1e2dfaad37893def62ec7
Tarun <shtarun9906@gmail.com>
2023-11-30 22:59:56 +0530

    added unique constraint and started cityRepository

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
+const CityRepository = require("./repository/city-repository");
 const setupAndStartServer = async () => {
   const app = express();
   app.use(express.json());
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`server started at ${process.env.PORT}`);
+    // console.log("city", City);
+    const repo = new CityRepository();
+    repo.createCity({ name: "LA" });
   });
 };
+
+setupAndStartServer();
---
+++ src/repository/city-repository.js
@@ -0,0 +1,20 @@
+const { City } = require("../models/index");
+
+class CityRepository {
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name });
+    } catch (e) {
+      throw { e };
+    }
+  }
+  async deleteCity(cityId) {
+    try {
+      await City.destroy({ where: { id: cityId } });
+    } catch (e) {
+      throw { e };
+    }
+  }
+}
+
+module.exports = CityRepository;
