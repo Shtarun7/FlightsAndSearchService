3270e8cdc10c00a7988a48657aa8d4da20159675
Tarun <shtarun9906@gmail.com>
2023-11-30 23:24:45 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,7 +4,9 @@ class CityRepository {
   async createCity({ name }) {
     try {
       const city = await City.create({ name });
+      return city;
     } catch (e) {
+      console.log("Something went wrong in the repository layer::create city");
       throw { e };
     }
   }
@@ -12,6 +14,27 @@ class CityRepository {
     try {
       await City.destroy({ where: { id: cityId } });
     } catch (e) {
+      console.log("error :: city repository: delete city");
+      throw { e };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (e) {
+      console.log("error::get city");
+      throw { e };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, { where: { id: cityId } });
+      return city;
+    } catch (e) {
+      console.log("error:: city repository:update city");
       throw { e };
     }
   }
